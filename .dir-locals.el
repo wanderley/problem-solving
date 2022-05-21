d79e5e93959df157f58ce74ed15f231764558ee9
Wanderley da Silva <wanderley.guimaraes@gmail.com>
2022-05-20 17:32:37 -0700

    Add Emacs setup for main folder

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((magit-mode
+  (mode . adaptive-wrap-prefix)
+  (mode . visual-fill-column)
+  (mode . visual-line)))
