a1627255262979a78e8f75c9d6ed64fa92838c06
Wanderley da Silva <wanderley.guimaraes@gmail.com>
2022-12-11 19:06:25 -0800

    site: compilation command to test changes

--- notes/.dir-locals.el
+++ notes/.dir-locals.el
@@ -6,7 +6,9 @@
                                                         default-directory ".dir-locals.el"))))
          (eval . (setq-local
                   org-roam-db-location (expand-file-name "org-roam.db"
-                                                         org-roam-directory)))))
+                                                         org-roam-directory)))
+         (eval . (setq-local
+                  compile-command (format "cd .. && NOTE=%s make iterate-site" (buffer-name))))))
  (magit-mode
   (mode . adaptive-wrap-prefix)
   (mode . visual-fill-column)
